8f69751d971704f0d0a763a76589f3c48f594664
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2024-09-04 12:45:02 +0100

    add next fonts

---
+++ kibaswap/fonts/font.ts
@@ -0,0 +1,11 @@
+import { Poppins, Rubik } from "next/font/google";
+
+export const poppins = Poppins({
+  weight: ["400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+});
+
+export const rubik = Rubik({
+  weight: ["400", "700", "800", "900"],
+  subsets: ["latin"],
+});
