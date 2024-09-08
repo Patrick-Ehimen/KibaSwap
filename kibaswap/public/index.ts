43245c91be25bd66d8559afcfae8fddffee7c9f1
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2024-09-08 21:51:30 +0100

    add assets

---
+++ kibaswap/public/index.ts
@@ -0,0 +1,21 @@
+import Logo from "./assets/logo.svg";
+import ArrowDownUp from "./assets/svgs/arrow-down-up.svg";
+import Send from "./assets/svgs/send.svg";
+import Creditcard from "./assets/svgs/credit-card.svg";
+import Coin from "./assets/svgs/coins.svg";
+import Pool from "./assets/svgs/pool.svg";
+import ArrowLeftRight from "./assets/svgs/arrow-left-right.svg";
+import Position from "./assets/svgs/position.svg";
+import AddPosition from "./assets/svgs/add-position.svg";
+
+export {
+  Logo,
+  ArrowDownUp,
+  Send,
+  Creditcard,
+  Coin,
+  Pool,
+  ArrowLeftRight,
+  Position,
+  AddPosition,
+};
