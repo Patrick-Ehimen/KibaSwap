62d680ad018ad844658cf3b17a617275c9cb5132
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2024-10-13 23:07:04 +0100

    Update next.config.mjs

--- kibaswap/next.config.mjs
+++ kibaswap/next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: [
+      "assets.coingecko.com",
+      "tokens.uniswap.org",
+      "arbitrum.foundation",
+      "raw.githubusercontent.com",
+      "s2.coinmarketcap.com",
+      "cryptologos.cc",
+    ],
+  },
+};
 
 export default nextConfig;
