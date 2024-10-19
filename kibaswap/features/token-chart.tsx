38edd34ab53904f730d87a9be7f59db2e386323a
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2024-10-19 15:39:11 +0100

    Create token-chart.tsx

---
+++ kibaswap/features/token-chart.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import React from "react";
+
+import TokenInfo from "@/components/token-info";
+import TokenBreadCrumb from "@/components/token-bread-crumb";
+import { useSwapContext } from "@/context/swap-context";
+
+import { tokenLists } from "@/constants/tokens-list";
+
+export default function TokenChart() {
+  const { toTokenAddress } = useSwapContext();
+
+  // Find the selected token from the tokenLists
+  const selectedToken = tokenLists.find(
+    (token) => token.address === toTokenAddress
+  );
+
+  return (
+    <main className="w-1/2 mr-[200px]">
+      <TokenBreadCrumb />
+      <TokenInfo token={selectedToken} />
+    </main>
+  );
+}
