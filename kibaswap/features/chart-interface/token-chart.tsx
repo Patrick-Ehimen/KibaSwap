8d0e2e08e545d725032b5f51ad69442ed477bdda
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2024-10-21 23:32:54 +0100

    ..

--- kibaswap/features/chart-interface/token-chart.tsx
+++ kibaswap/features/chart-interface/token-chart.tsx
@@ -2,10 +2,10 @@
 
 import React from "react";
 
-import TokenInfo from "@/components/token-info";
+import TokenInfo from "@/components/_components/token-info";
 import TokenBreadCrumb from "@/components/token-bread-crumb";
+import ChartInterface from "./chart-interface";
 import { useSwapContext } from "@/context/swap-context";
-
 import { tokenLists } from "@/constants/tokens-list";
 
 export default function TokenChart() {
@@ -20,6 +20,7 @@ export default function TokenChart() {
     <main className="w-1/2 mr-[200px]">
       <TokenBreadCrumb />
       <TokenInfo token={selectedToken} />
+      <ChartInterface />
     </main>
   );
 }
--- kibaswap/features/swap-interface/swap-interface.tsx
+++ kibaswap/features/swap-interface/swap-interface.tsx
@@ -7,7 +7,7 @@ export default function SwapInterface() {
     <main className="flex-grow flex items-center justify-center px-4 py-2 sm:px-6 lg:px-8">
       <div className="w-full max-w-md bg-[#353546] shadow-xl rounded-xl p-6">
         <div className="flex justify-between items-center mb-6">
-          <h2 className="text-2xl font-bold">Swap</h2>
+          <h2 className="text-xl font-bold">Swap Tokens</h2>
           <div className="flex space-x-2">
             <button className="p-2 rounded-xl bg-[#23242F]">
               <Settings2Icon size={20} />
