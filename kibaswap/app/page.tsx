407abe1661012a83a599e367168333a657fe2123
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2024-10-23 21:27:57 +0100

    Update page.tsx

--- kibaswap/app/page.tsx
+++ kibaswap/app/page.tsx
@@ -1,41 +1,27 @@
 import Image from "next/image";
 
+import TokenChart from "@/features/chart-interface/token-chart";
+import SwapInterface from "@/features/swap-interface/swap-interface";
+import { SwapProvider } from "@/context/swap-context";
+
 export default function Home() {
+  console.log("Rendering Home component");
   return (
-    <main>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-    </main>
+    <SwapProvider>
+      <main className="mt-20 mx-10 lg:mx-20 md:flex-col flex">
+        <div className="flex lg:flex-row flex-col">
+          <div className="lg:w-2/3 w-full">
+            {" "}
+            {/* TokenChart takes 65% on larger screens */}
+            <TokenChart />
+          </div>
+          <div className="lg:w-2/5 w-full mt-[30px]">
+            {" "}
+            {/* SwapInterface takes 35% on larger screens */}
+            <SwapInterface />
+          </div>
+        </div>
+      </main>
+    </SwapProvider>
   );
 }
