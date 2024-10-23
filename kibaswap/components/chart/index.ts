c1fde7451b1de587359d21884a95bc3e7426a94c
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2024-10-23 21:28:08 +0100

    Create index.ts

---
+++ kibaswap/components/chart/index.ts
@@ -0,0 +1,6 @@
+import DailyChart from "./daily-chart";
+import WeeklyChart from "./weekly-chart";
+import MonthlyChart from "./monthly-chart";
+import YearlyChart from "./yearly-chart";
+
+export { DailyChart, WeeklyChart, MonthlyChart, YearlyChart };
