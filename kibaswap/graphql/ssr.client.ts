09ef9f35aa7d6e1472d60df803fa457a4f6e6509
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2024-10-26 11:15:53 +0100

    delete graphl folder

--- kibaswap/graphql/ssr.client.ts
+++
@@ -1,6 +0,0 @@
-import { GraphQLClient } from "graphql-request";
-
-import { env } from "@/env/server";
-
-// this hits the graphql api exposed in the /app/graphql/route.ts file
-export const client = new GraphQLClient(env.ROOT_URI);
