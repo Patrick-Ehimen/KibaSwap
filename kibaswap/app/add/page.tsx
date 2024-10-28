aab47cdb292f891fe80f6b75171b1a3c4273f48d
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2024-10-28 20:20:58 +0100

    delete pages

--- kibaswap/app/add/page.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function AddPosition() {
-  return <div>page</div>;
-}
--- kibaswap/app/dao/delegates/delegate.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Delegate() {
-  return <div>delegate</div>;
-}
