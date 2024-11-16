d06c0e3d707576a0113e3690ed44f046221a04b0
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2024-11-16 16:38:01 +0100

    Update page.tsx

--- kibaswap/app/explore/page.tsx
+++ kibaswap/app/explore/page.tsx
@@ -1,5 +1,25 @@
+"use client";
+
 import React from "react";
 
+import BitcoinChart from "@/features/markets/market-charts/bitcoin-chart";
+import EthereumChart from "@/features/markets/market-charts/ethereum-chart";
+import UsdtChart from "@/features/markets/market-charts/usdt-chart";
+import SolanaChart from "@/features/markets/market-charts/solana-chart";
+import RadialChart from "@/features/markets/market-charts/radial-chart";
+
 export default function Tokens() {
-  return <div>page</div>;
+  return (
+    <main className="mx-10 lg:mx-20 my-10">
+      <div className="hidden md:flex">
+        <BitcoinChart />
+        <EthereumChart />
+        <UsdtChart />
+        <SolanaChart />
+        <div>
+          <RadialChart />
+        </div>
+      </div>
+    </main>
+  );
 }
