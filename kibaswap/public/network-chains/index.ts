6934febbb19bc2852a227564b5157df8a27539de
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2024-11-21 11:00:18 +0100

    Create index.ts

---
+++ kibaswap/public/network-chains/index.ts
@@ -0,0 +1,33 @@
+import Aptos from "./Aptos.svg";
+import Arbitrum from "./Arbitrum.svg";
+import Avalance from "./Avalance.svg";
+import Binance from "./Binance.svg";
+import Cardano from "./Cardano.svg";
+import Celo from "./Celo.svg";
+import EOS from "./EOS.svg";
+import Ethereum from "./Ethereum.svg";
+import Fantom from "./Fantom.svg";
+import Flow from "./Flow.svg";
+import Kadena from "./Kadena.svg";
+import Okex from "./Okex.svg";
+import Polkadot from "./Polkadot.svg";
+import Polygon from "./Polygon.svg";
+import Solana from "./Solana.svg";
+
+export {
+  Aptos,
+  Arbitrum,
+  Avalance,
+  Binance,
+  Cardano,
+  Celo,
+  EOS,
+  Ethereum,
+  Fantom,
+  Flow,
+  Kadena,
+  Okex,
+  Polkadot,
+  Polygon,
+  Solana,
+};
