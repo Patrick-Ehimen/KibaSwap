be2401515261ab3a702d258b85b3e66981fd05e3
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2024-12-05 22:45:07 +0100

    Update swap-interface.tsx

--- kibaswap/features/swap-interface/swap-interface.tsx
+++ kibaswap/features/swap-interface/swap-interface.tsx
@@ -5,7 +5,7 @@ import TokenSwap from "@/components/_components/token-swap";
 export default function SwapInterface() {
   return (
     <main className="flex-grow flex items-center justify-center px-4 py-2 sm:px-6 lg:px-8">
-      <div className="w-full max-w-md md:mt-10 bg-slate-400 dark:bg-[#353546] shadow-xl rounded-xl p-6">
+      <div className="w-full max-w-md md:mt-10 bg-[#353546] shadow-xl rounded-xl p-6">
         <div className="flex justify-between items-center mb-6">
           <h2 className="text-xl font-bold">Swap Tokens</h2>
           <div className="flex space-x-2">
